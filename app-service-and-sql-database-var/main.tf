c7698573d579b1003b97d46889197c3cb07198d6
Sacha Wharton <sacha.wharton@reponsive.co.za>
2020-06-03 15:35:58 +0200

    changed the login name

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -46,7 +46,7 @@ resource "azurerm_sql_server" "test" {
   resource_group_name          = azurerm_resource_group.RG-Terraform.name
   location                     = azurerm_resource_group.RG-Terraform.location
   version                      = "12.0"
-  administrator_login          = "houssem"
+  administrator_login          = "sacha.wharton"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
